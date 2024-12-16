a84aba08022eb1380b2ce0c49603b4e9175ef916
arif <arifalaudinmasindra@gmail.com>
2024-12-16 18:42:44 +0800

    Completed the website

---
+++ script.js
@@ -0,0 +1,45 @@
+const textBox = document.getElementById("textBox");
+const fromFahrenheit = document.getElementById("fromFahrenheit");
+const fromCelsius = document.getElementById("fromCelsius");
+const fromKelvin = document.getElementById("fromKelvin");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const toKelvin = document.getElementById("toKelvin");
+const convertBtn = document.getElementById("convertBtn");
+const output = document.getElementById("output");
+
+convertBtn.onclick = function(){
+    const numInput = textBox.value;
+
+    let numOutput = "Invalid Conversion";
+
+    if (fromFahrenheit.checked && toFahrenheit.checked) {
+        numOutput = numInput;
+    }
+    else if (fromFahrenheit.checked && toCelsius.checked) {
+        numOutput = ((numInput - 32) * 5) / 9;
+    }
+    else if (fromFahrenheit.checked && toKelvin.checked) {
+        numOutput = ((numInput - 32) * 5) / 9 + 273.15;
+    }
+    else if (fromCelsius.checked && toCelsius.checked) {
+        numOutput = numInput;
+    }
+    else if (fromCelsius.checked && toFahrenheit.checked) {
+        numOutput = (numInput * 9) / 5 + 32;
+    }
+    else if (fromCelsius.checked && toKelvin.checked) {
+        numOutput = numInput + 273.15;
+    }
+    else if (fromKelvin.checked && toKelvin.checked) {
+        numOutput = numInput;
+    }
+    else if (fromKelvin.checked && toFahrenheit.checked) {
+        numOutput = ((numInput - 273.15) * 9) / 5 + 32;
+    }
+    else if (fromKelvin.checked && toCelsius.checked) {
+        numOutput = numInput - 273.15;
+    }
+
+    output.textContent = `Converted Value: ${numOutput}`;
+};
